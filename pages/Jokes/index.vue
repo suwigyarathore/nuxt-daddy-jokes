e26395e113c3cd350b700ddfa32a5dcc239bbd07
Suwigya Ratrhore <suwigya.rathore.ui@gmail.com>
2019-07-04 13:20:09 +0200

    Final commit of Nuxt crash course

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <SearchJokes v-on:search-text="searchText"/>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke"/>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Joke from "../../components/Joke";
+import SearchJokes from "../../components/SearchJokes";
+
+export default {
+  components: {
+    Joke,
+    SearchJokes
+  },
+  data() {
+    return {
+      jokes: []
+    };
+  },
+
+  methods: {
+    async searchText(text) {
+      const config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+
+      try {
+        const res = await axios.get(
+          `https://icanhazdadjoke.com/search?term=${text}`,
+          config
+        );
+        this.jokes = res.data.results;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  },
+
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      this.jokes = res.data.results;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  head() {
+    return {
+      title: "Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
